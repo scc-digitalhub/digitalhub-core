d2144997a5128220a40bf5194803256f7b9e6f37
Matteo Saloni <matteo.saloni@gmail.com>
2025-10-14 16:53:37 +0200

    feat: k8s serve goes to RUNNING when pod is Ready

--- modules/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/infrastructure/monitor/K8sServeMonitor.java
+++ modules/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/infrastructure/monitor/K8sServeMonitor.java
@@ -103,7 +103,7 @@ public class K8sServeMonitor extends K8sBaseMonitor<K8sServeRunnable> {
                 if (K8sRunnableState.PENDING.name().equals(runnable.getState()) && pods != null) {
                     boolean running = pods
                         .stream()
-                        .anyMatch(p -> p.getStatus() != null && "Running".equals(p.getStatus().getPhase()));
+                        .anyMatch(p -> p.getStatus() != null && "Ready".equals(p.getStatus().getPhase()));
                     if (running) {
                         runnable.setState(K8sRunnableState.RUNNING.name());
                     }
