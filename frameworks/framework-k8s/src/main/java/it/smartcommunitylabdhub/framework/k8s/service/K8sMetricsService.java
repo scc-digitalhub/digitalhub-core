dfec18569ea465b240289f1ed99de3daafd683a8
matteo-s <matteo.saloni@gmail.com>
2026-04-02 17:33:42 +0200

    feat: add pvc and pods to k8s metrics

--- frameworks/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/service/K8sMetricsService.java
+++ frameworks/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/service/K8sMetricsService.java
@@ -7,16 +7,21 @@ import io.kubernetes.client.Metrics;
 import io.kubernetes.client.custom.ContainerMetrics;
 import io.kubernetes.client.custom.PodMetrics;
 import io.kubernetes.client.custom.Quantity;
+import io.kubernetes.client.custom.Quantity.Format;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1PersistentVolumeClaim;
 import it.smartcommunitylabdhub.commons.config.ApplicationProperties;
 import it.smartcommunitylabdhub.commons.exceptions.StoreException;
 import it.smartcommunitylabdhub.framework.k8s.kubernetes.K8sLabelHelper;
 import jakarta.annotation.Nonnull;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
@@ -97,10 +102,13 @@ public class K8sMetricsService implements InitializingBean {
             return null;
         }
 
+        Entry<String, String> label = k8sLabelHelper.buildCoreLabel(key, value);
+
         try {
             ContainerMetrics metrics = new ContainerMetrics();
             metrics.setName(key);
             metrics.setUsage(new HashMap<>());
+            BigDecimal big1 = new BigDecimal(1);
 
             //aggregate metrics for all pods matching
             List<PodMetrics> podMetrics = metricsApi
@@ -115,10 +123,28 @@ public class K8sMetricsService implements InitializingBean {
                 .toList();
 
             podMetrics.forEach(pm -> {
+                //count pods
+                if (metrics.getUsage().containsKey("pods")) {
+                    Quantity existing = metrics.getUsage().get("pods");
+                    metrics.getUsage().put("pods", new Quantity(existing.getNumber().add(big1), Format.DECIMAL_SI));
+                } else {
+                    metrics.getUsage().put("pods", new Quantity(big1, Format.DECIMAL_SI));
+                }
+
                 if (pm.getContainers() != null) {
                     pm
                         .getContainers()
                         .forEach(cm -> {
+                            //count containers
+                            if (metrics.getUsage().containsKey("containers")) {
+                                Quantity existing = metrics.getUsage().get("containers");
+                                metrics
+                                    .getUsage()
+                                    .put("containers", new Quantity(existing.getNumber().add(big1), Format.DECIMAL_SI));
+                            } else {
+                                metrics.getUsage().put("containers", new Quantity(big1, Format.DECIMAL_SI));
+                            }
+
                             if (cm.getUsage() != null) {
                                 log.trace(
                                     "Adding metrics for {} {} pod {} container {}: {}",
@@ -173,6 +199,74 @@ public class K8sMetricsService implements InitializingBean {
                         });
                 }
             });
+
+            //fetch pvc metrics for matching pods
+
+            try {
+                String labelSelector = label.getKey() + "=" + label.getValue();
+                List<V1PersistentVolumeClaim> pvcs = coreV1Api
+                    .listNamespacedPersistentVolumeClaim(
+                        namespace,
+                        null,
+                        null,
+                        null,
+                        null,
+                        labelSelector,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                    .getItems();
+
+                pvcs.forEach(pvc -> {
+                    Optional
+                        .ofNullable(pvc.getStatus())
+                        .map(s -> s.getCapacity())
+                        .map(m -> m.get("storage"))
+                        .ifPresent(c -> {
+                            log.trace(
+                                "Adding PVC metrics for {} {} pvc {}: {}",
+                                key,
+                                value,
+                                pvc.getMetadata().getName(),
+                                c
+                            );
+
+                            if (metrics.getUsage().containsKey("disk")) {
+                                //we expect format to match, or skip
+                                try {
+                                    Quantity existing = metrics.getUsage().get("disk");
+                                    if (existing.getFormat().equals(c.getFormat())) {
+                                        metrics
+                                            .getUsage()
+                                            .put(
+                                                "disk",
+                                                new Quantity(existing.getNumber().add(c.getNumber()), c.getFormat())
+                                            );
+                                    }
+                                } catch (NumberFormatException e) {
+                                    log.warn(
+                                        "Error parsing disk metric for {} {} pvc {}: {}",
+                                        key,
+                                        value,
+                                        pvc.getMetadata().getName(),
+                                        e.getMessage()
+                                    );
+                                }
+                            } else {
+                                metrics.getUsage().put("disk", c);
+                            }
+                        });
+                });
+            } catch (ApiException e) {
+                log.error("Error fetching pod details for metrics: {}", e.getMessage());
+                if (log.isTraceEnabled()) {
+                    log.trace("k8s api response: {}", e.getResponseBody());
+                }
+            }
 
             return metrics;
         } catch (ApiException e) {
