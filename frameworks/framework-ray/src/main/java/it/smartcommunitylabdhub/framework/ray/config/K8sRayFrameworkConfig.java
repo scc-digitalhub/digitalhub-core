7b6e9ad9dd227f7dabc884a80a3797f4c65479b5
kazhamiakin <kazhamiakin@gmail.com>
2026-05-19 22:27:37 +0200

    API alignment

--- frameworks/framework-ray/src/main/java/it/smartcommunitylabdhub/framework/ray/config/K8sRayFrameworkConfig.java
+++ frameworks/framework-ray/src/main/java/it/smartcommunitylabdhub/framework/ray/config/K8sRayFrameworkConfig.java
@@ -9,6 +9,7 @@ package it.smartcommunitylabdhub.framework.ray.config;
 import io.kubernetes.client.openapi.ApiClient;
 import it.smartcommunitylabdhub.commons.config.YamlPropertySourceFactory;
 import it.smartcommunitylabdhub.framework.k8s.annotations.ConditionalOnKubernetes;
+import it.smartcommunitylabdhub.framework.k8s.jackson.KubernetesMapper;
 import it.smartcommunitylabdhub.framework.ray.infrastructure.k8s.K8sRayJobFramework;
 import it.smartcommunitylabdhub.framework.ray.infrastructure.k8s.K8sRayServiceFramework;
 import it.smartcommunitylabdhub.framework.ray.listeners.K8sRayJobListener;
@@ -21,6 +22,7 @@ import it.smartcommunitylabdhub.runtimes.store.RunnableStoreImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @PropertySource(value = "classpath:/framework-ray.yml", factory = YamlPropertySourceFactory.class)
@@ -29,16 +31,25 @@ public class K8sRayFrameworkConfig {
     // ---- runnable stores ----
     @Bean
     @ConditionalOnKubernetes
-    public RunnableStore<K8sRayJobRunnable> k8sRayJobRunnableStore(RunnableRepository runnableRepository) {
-        return new RunnableStoreImpl<>(K8sRayJobRunnable.class, runnableRepository);
+    public RunnableStore<K8sRayJobRunnable> k8sRayJobRunnableStoreService(
+            RunnableRepository runnableRepository,
+            PlatformTransactionManager transactionManager) {
+        RunnableStoreImpl<K8sRayJobRunnable> store = new RunnableStoreImpl<>(
+                K8sRayJobRunnable.class, runnableRepository, transactionManager);
+        store.setObjectMapper(KubernetesMapper.CBOR_OBJECT_MAPPER);
+        return store;
     }
-
     @Bean
     @ConditionalOnKubernetes
-    public RunnableStore<K8sRayServiceRunnable> k8sRayServiceRunnableStore(RunnableRepository runnableRepository) {
-        return new RunnableStoreImpl<>(K8sRayServiceRunnable.class, runnableRepository);
+    public RunnableStore<K8sRayServiceRunnable> k8sRayServiceRunnableStoreService(
+            RunnableRepository runnableRepository,
+            PlatformTransactionManager transactionManager) {
+        RunnableStoreImpl<K8sRayServiceRunnable> store = new RunnableStoreImpl<>(
+                K8sRayServiceRunnable.class, runnableRepository, transactionManager);
+        store.setObjectMapper(KubernetesMapper.CBOR_OBJECT_MAPPER);
+        return store;
     }
-
+    
     // ---- frameworks ----
 
     @Bean
